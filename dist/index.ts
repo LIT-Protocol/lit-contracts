b9ab84c50504930c32656ac7629922398cb37a3e
Anson <ansonox@gmail.com>
2024-07-18 21:49:46 +0100

    feat: add package config and auto pub CI

---
+++ dist/index.ts
@@ -0,0 +1,15 @@
+// dev
+export { default as _cayenne } from "./dev/cayenne.json";
+export { default as _datilDev } from "./dev/datil-dev.json";
+export { default as _datilTest } from "./dev/datil-test.json";
+export { default as _habanero } from "./dev/habanero.json";
+export { default as _internalDev } from "./dev/internalDev.json";
+export { default as _manzano } from "./dev/manzano.json";
+
+// prod
+export { default as cayenne } from "./prod/cayenne.json";
+export { default as datilDev } from "./prod/datil-dev.json";
+export { default as datilTest } from "./prod/datil-test.json";
+export { default as habanero } from "./prod/habanero.json";
+export { default as internalDev } from "./prod/internalDev.json";
+export { default as manzano } from "./prod/manzano.json";
