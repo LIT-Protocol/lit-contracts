ab7a0acc7db8b2a579b72bd9f96f227fa4c81664
Anson <ansonox@gmail.com>
2024-07-18 23:31:55 +0100

    fix: exports

--- dist/index.ts
+++ dist/index.ts
@@ -1,12 +1,14 @@
 // dev
-export { cayenne as _cayenne } from "./dev/cayenne";
+export { datil as _datil } from "./dev/datil";
 export { datilDev as _datilDev } from "./dev/datil-dev";
 export { datilTest as _datilTest } from "./dev/datil-test";
+export { cayenne as _cayenne } from "./dev/cayenne";
 export { habanero as _habanero } from "./dev/habanero";
 export { internalDev as _internalDev } from "./dev/internalDev";
 export { manzano as _manzano } from "./dev/manzano";
 
 // prod
+export { datil } from "./prod/datil";
 export { cayenne } from "./prod/cayenne";
 export { datilDev } from "./prod/datil-dev";
 export { datilTest } from "./prod/datil-test";
