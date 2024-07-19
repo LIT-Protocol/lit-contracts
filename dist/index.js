1063b4e799de787a6e9ad6cb031e0eebf7a0a16e
Anson <ansonox@gmail.com>
2024-07-19 14:01:12 +0100

    feat: remove esbuild as need absolute path on index.js

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,17 @@
-"use strict";
-export { datil as _datil } from "./dev/datil";
-export { datilDev as _datilDev } from "./dev/datil-dev";
-export { datilTest as _datilTest } from "./dev/datil-test";
-export { cayenne as _cayenne } from "./dev/cayenne";
-export { habanero as _habanero } from "./dev/habanero";
-export { internalDev as _internalDev } from "./dev/internalDev";
-export { manzano as _manzano } from "./dev/manzano";
-export { datil } from "./prod/datil";
-export { cayenne } from "./prod/cayenne";
-export { datilDev } from "./prod/datil-dev";
-export { datilTest } from "./prod/datil-test";
-export { habanero } from "./prod/habanero";
-export { internalDev } from "./prod/internalDev";
-export { manzano } from "./prod/manzano";
+// dev
+export { datil as _datil } from "./dev/datil.js";
+export { datilDev as _datilDev } from "./dev/datil-dev.js";
+export { datilTest as _datilTest } from "./dev/datil-test.js";
+export { cayenne as _cayenne } from "./dev/cayenne.js";
+export { habanero as _habanero } from "./dev/habanero.js";
+export { internalDev as _internalDev } from "./dev/internalDev.js";
+export { manzano as _manzano } from "./dev/manzano.js";
+
+// prod
+export { datil } from "./prod/datil.js";
+export { cayenne } from "./prod/cayenne.js";
+export { datilDev } from "./prod/datil-dev.js";
+export { datilTest } from "./prod/datil-test.js";
+export { habanero } from "./prod/habanero.js";
+export { internalDev } from "./prod/internalDev.js";
+export { manzano } from "./prod/manzano.js";
